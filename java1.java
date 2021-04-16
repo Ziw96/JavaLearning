10e3a22f83683dbd63a4a3f958528ee74f417348
ziwei96 <zl2853@columbia.edu>
2021-04-16 16:12:19 +0800

    complete code
    complete first read and write java code

--- java1.java
+++ java1.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 
 public class java1 {
     public static void main(String[] args) {
+        Scanner inputScanner;
+        Double num1;
 
+        inputScanner = new Scanner(System.in);
+        num1 = inputScanner.nextDouble();
+        System.out.println("the number you input is"+num1);
+        System.exit(0);
     }
 
 }
