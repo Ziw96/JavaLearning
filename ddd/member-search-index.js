6db0562ed9ea082bdf794003897e1880009a4a14
ziwei96 <zl2853@columbia.edu>
2021-04-19 20:05:26 +0800

    add git ignore and first javadoc
    add git ignore and first javadoc

---
+++ ddd/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"java1","l":"java1()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"java1","l":"main(String[])","url":"main(java.lang.String[])"}]
--- java1.java
+++ java1.java
@@ -1,10 +1,20 @@
+/**
+ This is to comment the whole doc, at the beginning of doc
+ @author ZiweiLi
+ @version 0.0
+ */
+
 import java.util.Scanner;
+// this is to comment one line of code;
 
 public class java1 {
     public static void main(String[] args) {
         Scanner inputScanner;
         Double num1;
-
+        /*
+        this is to comment multi
+        lines of code
+        */
         inputScanner = new Scanner(System.in);
         num1 = inputScanner.nextDouble();
         System.out.println("the number you input is"+num1);
