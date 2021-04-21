e85edb38d459c1acf43443203ba655359c9a61bf
ziwei96 <zl2853@columbia.edu>
2021-04-21 11:22:15 +0800

    update exercise1 to switch

--- exercise1.java
+++ exercise1.java
@@ -1,6 +1,6 @@
 public class exercise1 {
 
-    public static void main(String[] args){
+/*    public static void main(String[] args){
         int a=3;
         int x=100;
         if (a==1) {
@@ -11,6 +11,28 @@ public class exercise1 {
             x+=16;
         } else {
             x+=34;
+        }
+        System.out.println(x);
+    }
+    */
+    public static void main(String[] args){
+        int a=3;
+        int x=100;
+
+        switch (a) {
+            case 1 :
+                x+=5;
+                break;
+            case 2:
+
+                x+=10;
+                break;
+            case 3:
+                x+=16;
+                break;
+            default:
+                x+=34;
+                break;
         }
         System.out.println(x);
     }
