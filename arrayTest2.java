cd8a9f7f0785ed3cc0b27046700b254fc4c27e29
Ziw96 <ziwei.ricky.li@foxmail.com>
2021-04-22 23:26:08 +0800

    add comment in arraytest2

---
+++ arrayTest2.java
@@ -0,0 +1,14 @@
+public class arrayTest2 {
+    public static void main(String[] args) {
+        /*test script for 2-D array
+        */
+        int[][] ids = {{1,2,3},{4,5},{8,9,0}};
+
+        for (int i=0;i<ids.length;i++){
+            for (int j=0;j<ids[i].length;j++){
+                System.out.print(ids[i][j]+"\t");
+            }
+        System.out.println();
+        }
+    }
+}
